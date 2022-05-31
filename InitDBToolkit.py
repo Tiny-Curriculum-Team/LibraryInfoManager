6fa7fda1350895388352a52f3ddaf30061adc1c7
BreezeShane <346869441@qq.com>
2022-05-31 20:59:49 +0800

    Modified check_existence_of_database function in order to preserve the origin database from misdeleting and added an asynchronous function to initialize data after initializing database.

--- InitDBToolkit.py
+++ InitDBToolkit.py
@@ -7,19 +7,35 @@ def check_existence_of_database():
     init_cursor = init_connection.cursor()
     init_cursor.execute("show databases like 'LIMS';")
     result = init_cursor.fetchall()
-    if not result:
-        init_cursor.execute("create database LIMS CHARACTER SET utf8 COLLATE utf8_general_ci;")
+    if result:
+        answer = input("Database LIMS exists, want to remove it?[y/N]\n")
+        if answer == 'y':
+            init_cursor.execute('drop database LIMS;')
+        else:
+            print('Nothing done.')
+            return False
+    init_cursor.execute("create database LIMS CHARACTER SET utf8 COLLATE utf8_general_ci;")
     disconnect_db(init_connection)
+    return True
 
 
-def init_db():
+def init_db(data_initialize):
     check_existence_of_database()
     database_connection = connect_to_db(database='LIMS')
     cursor = database_connection.cursor()
     execute_sql_script(cursor, join('./SQLs', 'InitDatabase.sql'))
     database_connection.commit()
     disconnect_db(database_connection)
+    data_initialize()
+
+
+def init_data():
+    database_connection = connect_to_db(database='LIMS')
+    cursor = database_connection.cursor()
+    execute_sql_script(cursor, join('./SQLs', 'InitData.sql'))
+    database_connection.commit()
+    disconnect_db(database_connection)
 
 
 if __name__ == '__main__':
-    init_db()
+    init_db(init_data)
