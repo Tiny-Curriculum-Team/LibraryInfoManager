6e2d9cca8d749395fd30cb529b7f2f13ba63bbd6
BreezeShane <346869441@qq.com>
2022-06-02 21:55:56 +0800

    Removed the comment.

--- Django/Users/views.py
+++ Django/Users/views.py
@@ -37,22 +37,21 @@ def user_logout(request):
 
 # 用户注册
 def user_register(request):
-    pass
-    # if request.method == 'POST':
-    #     user_register_form = UserRegisterForm(data=request.POST)
-    #     if user_register_form.is_valid():
-    #         new_user = user_register_form.save(commit=False)
-    #         # 设置密码
-    #         new_user.set_password(user_register_form.cleaned_data['password'])
-    #         new_user.save()
-    #         # 保存好数据后立即登录并返回博客列表页面
-    #         login(request, new_user)
-    #         return redirect("article:article_list")
-    #     else:
-    #         return HttpResponse("注册表单输入有误。请重新输入~")
-    # elif request.method == 'GET':
-    #     user_register_form = UserRegisterForm()
-    #     context = { 'form': user_register_form }
-    #     return render(request, 'static/signup.html', context)
-    # else:
-    #     return HttpResponse("请使用GET或POST请求数据")
+    if request.method == 'POST':
+        user_register_form = UserRegisterForm(data=request.POST)
+        if user_register_form.is_valid():
+            new_user = user_register_form.save(commit=False)
+            # 设置密码
+            new_user.set_password(user_register_form.cleaned_data['password'])
+            new_user.save()
+            # 保存好数据后立即登录并返回博客列表页面
+            login(request, new_user)
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("注册表单输入有误。请重新输入~")
+    elif request.method == 'GET':
+        user_register_form = UserRegisterForm()
+        context = { 'form': user_register_form }
+        return render(request, 'static/signup.html', context)
+    else:
+        return HttpResponse("请使用GET或POST请求数据")
