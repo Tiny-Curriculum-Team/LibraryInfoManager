56ee3ddadb720cc472d4a1bbddcad07eeb7b6dbc
BreezeShane <346869441@qq.com>
2022-06-04 17:06:29 +0800

    Fixed up the Initialize shell script.

--- Initialize.sh
+++ Initialize.sh
@@ -1,3 +1,3 @@
 python Django/manage.py makemigrations
 python Django/manage.py migrate
-python createsuperuser --UserID 0 --name root --nickname root --password bassword --tel 15904572408
+python Django/manage.py createsuperuser --UserID 0 --name root --nickname root --password bassword --tel 15904572408
