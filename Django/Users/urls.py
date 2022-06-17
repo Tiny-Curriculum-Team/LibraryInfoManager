2e497330b25de441eec95d832e2cd41d4e86909e
BreezeShane <346869441@qq.com>
2022-06-17 15:56:30 +0800

    Finished user profile module.

--- Django/Users/urls.py
+++ Django/Users/urls.py
@@ -7,9 +7,13 @@ app_name = 'users'
 urlpatterns = [
     path('sign_in/', views.user_login, name='signin'),
     path('sign_up/', views.user_register, name='signup'),
+    path('logout/', views.logout, name='logout'),
     path('manage/', views.user_manage, name='userManage'),
     path('manage/post_del_user/', views.remove_user, name='userRemove'),
     path('manage/post_H_del_user/', views.restore_user, name='userRestore'),
     path('manage/get_query_user_info/', views.pull_query_user_info, name='getUserInfo'),
     path('manage/post_update_user_info/', views.update_user, name='postUserInfo'),
+    path('profile/', views.user_profile_view, name='userProfile'),
+    path('post_edited_profile/', views.update_user_profile, name="updateUserProfile"),
+    path('post_edited_password/', views.update_user_password, name="updateUserPassword")
 ]
